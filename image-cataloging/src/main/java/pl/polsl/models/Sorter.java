b0b286aabbf18e814e8fab9de66eba330a8e43e4
MichasSlusarczyk <michas.slusarczyk@gmail.com>
2021-11-27 03:04:18 +0100

    feat: create sorter class

---
+++ image-cataloging/src/main/java/pl/polsl/models/Sorter.java
@@ -0,0 +1,13 @@
+package pl.polsl.models;
+import java. util.*;
+
+public class Sorter {
+    public void group(List<String> imagePaths)
+    {
+        List<String> hashcodes = getHashcodes(imagePaths);
+    }
+    private List<String> getHashcodes(List<String> imagePaths)
+    {
+        return null;
+    }
+}
